d71e16840ccb0fd66aaa03681c54cd2dfb4c9630
SeokJuK <ksjksj0505@naver.com>
2023-02-15 20:36:14 +0900

    게시판 수정 및 삭제, alert

--- src/main/java/com/study/board/Service/BoardService.java
+++ src/main/java/com/study/board/Service/BoardService.java
@@ -5,13 +5,36 @@ import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BoardService {
 
     @Autowired //Spring에서는 알아서 읽어와 객체를 주입해준다
     private BoardRepository boardRepository;
+
+    //글 작성 처리
     public void write(Board board){
 
         boardRepository.save(board);
     }
+
+    //글 불러올 method(게시글 리스트 처리)
+    public List<Board> boardList(){
+
+        return boardRepository.findAll(); //board 클래스가 담긴 List를 반환
+    }
+
+    //특정 게시글 불러오기
+    public Board boardView(Integer id){
+
+        return boardRepository.findById(id).get();
+    }
+
+    //특정 게시글 삭제
+    public void boardDelete(Integer id){
+
+        boardRepository.deleteById(id);
+    }
+
 }
--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -4,7 +4,9 @@ import com.study.board.Service.BoardService;
 import com.study.board.entity.Board;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -18,10 +20,64 @@ public class BoardController {
         return "boardwrite"; //어떤 html파일로 보내줄지
     }
     @PostMapping("/board/writepro") //form tag의 action과 일치해야한다
-    public String boardWritePro(Board board){ //entity형식 그대로 받아올 수 있다
+    public String boardWritePro(Board board, Model model){ //entity형식 그대로 받아올 수 있다 model은 글 작성이 완료되었다는 문구 표시를 위한
 
         boardService.write(board);
+        
+        // 글 작성이 완료되었다는 문구 출력
+        model.addAttribute("message", "글 작성이 완료되었습니다.");
+        model.addAttribute("searchUrl","/board/list");
 
-        return "";
+        return "message";
     }
+    //게시글 리스트
+    @GetMapping("/board/list")
+    public String boardList(Model model){       //데이터를 담아서 페이지로 보내주는 역할 = model
+
+        model.addAttribute("list", boardService.boardList()); //list라는 이름으로 메소드의 반환값을 넘기겠다
+
+        return "boardlist";
+    }
+
+    //게시글 상세 페이지
+    @GetMapping("/board/view") // localhost:8080/board/view?id=1 -> 1번째 게시글 불러온다
+    public String boardView(Model model, Integer id){
+
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardview";
+    }
+
+    //게시글 삭제
+    @GetMapping("/board/delete")
+    public String boardDelete(Integer id){
+
+        boardService.boardDelete(id);
+
+        return "redirect:/board/list";
+    }
+    //수정 페이지
+    @GetMapping("/board/modify/{id}")
+    public String boardModify(@PathVariable("id") Integer id,
+                                                    Model model){
+        //url이 들어왔을 때 뒤의 id가 @PathVariable에 인식이 되서 Integer형태의 id가 된다
+
+        //값을 넘겨줌
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardmodify";
+    }
+
+    //버튼 누르면 수정되게
+    @PostMapping("/board/update/{id}")
+    public String boardUpdate(@PathVariable("id") Integer id, Board board, Model model){  //Board객체를 만든 이유는 id와 content값을 불러와야 해서
+
+        Board boardTemp = boardService.boardView(id); //기존에 있던 글 검색
+        boardTemp.setTitle(board.getTitle());   //새로운 내용 덮어 씌우기
+        boardTemp.setContent(board.getContent());
+        model.addAttribute("message", "수정되었습니다");
+        model.addAttribute("searchUrl", "/board/list");
+        boardService.write(boardTemp); //저장
+
+        return "message";
+    }
+
 }
